a489d9d000fbc3454f6d3c0263b20d75cf43ea35
NathanIng <ing.nathany@gmail.com>
2015-09-01 11:39:59 -0700

    l

--- colordeconvolution.py
+++ colordeconvolution.py
@@ -1,2 +1,21 @@
 # Color Deconvolution from 3-channel - to - 3-channel images
+# Original by MaZ@cshs.org
+#
+# Reference: Ruifrok, A.C., et al. "Quantification of histochemical staining by color deconvolution"
+
+import numpy as np
+import skimage as ski
+
+
+def initializeMatrices:
+    MODx = np.array([0.644211, 0.092789, 0.6359544])
+    MODy = np.array([0.716556, 0.954111, 0.0010])
+    MODz = np.array([0.266844, 0.283111, 0.7717266])
+
+    
+
+
+def colorDeconvolution(image):
+    pass
+
 
